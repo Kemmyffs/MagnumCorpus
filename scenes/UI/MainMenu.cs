1125eb56f5543987e4a5de204aa6860a235a6e51
Kemmyffs <coufalmatejx@gmail.com>
2026-04-16 14:34:55 +0200

    feat: Menu update (tutorial)

--- scenes/UI/MainMenu.cs
+++ scenes/UI/MainMenu.cs
@@ -17,7 +17,11 @@ public partial class MainMenu : Control
 	//Connectnout v Godotu a potom udělat public void funkci se stejným jménem
 	public void _on_button_button_up()
 	{
-		Console.WriteLine("Zmack");
 		GetTree().ChangeSceneToFile("res://objects/rooms/dungeon_generator.tscn");
+	}
+
+	public void OnStartTutorialButtonUp()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/TutorialLevel.tscn");
 	}
 }
